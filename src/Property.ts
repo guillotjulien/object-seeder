16843800a9b84001136ac5821a75db9b74deadd7
Eressea <eressea6666@gmail.com>
2020-05-10 12:29:31 +0200

    Feature: Added new Property decorator call signatures

--- src/Property.ts
+++ src/Property.ts
@@ -1,3 +1,5 @@
+import { PropertyOptions } from './PropertyOptions';
+
 /**
  * Key used to register type metadata in the class instance at runtime.
  *
@@ -6,8 +8,8 @@
 export const PARAMETER_KEY = 'model:properties';
 
 /**
- * This is the @Property() decorator factory. This decorator, used on a class property,
- * retrieve the name and type of the property and add them to the class metadata.
+ * This decorator, used on a class property, retrieve the name and type of the
+ * property and add them to the class metadata.
  *
  * @param type Type function used to define a custom type. This parameter is used
  * to bypass current reflection API limitations where type cannot be reflected for
@@ -15,18 +17,20 @@ export const PARAMETER_KEY = 'model:properties';
  *
  * @see {@link https://github.com/microsoft/TypeScript/issues/4521|Issue} for further information.
  *
- * @example
- * // Results in { stringProperty: [Function: String] }
- * class Example {
- *      @Property()
- *      public stringProperty: string;
- * }
+ * @param options Options used to fine tune decorator behavior.
  *
  * @example
  * // Results in { userInfo: [Function: anonymous] }
  * class Example {
  *      @Property(() => UserInfo)
  *      public userInfo: string;
+ * }
+ *
+ * @example
+ * // Results in { stringProperty: [Function: String] }
+ * class Example {
+ *      @Property()
+ *      public stringProperty: string;
  * }
  *
  * Beware that with this decorator, a private property can be seeded as well,
@@ -38,7 +42,15 @@ export const PARAMETER_KEY = 'model:properties';
  *
  * @returns An instance of the decorator
  */
-export function Property(type?: () => Function): Function {
+export function Property(type: () => Function, options?: PropertyOptions): Function;
+export function Property(options: PropertyOptions): Function;
+export function Property(): Function;
+
+/**
+ * This decorator, used on a class property, retrieve the name and type of the
+ * property and add them to the class metadata.
+ */
+export function Property(typeOrOption?: (() => Function) | PropertyOptions, options?: PropertyOptions): Function {
     return (target: Record<string, any>, property: string): void => {
         const reflectedType = Reflect.getMetadata('design:type', target, property);
         const existingParameters = Reflect.getMetadata(PARAMETER_KEY, target) || {};
@@ -50,7 +62,7 @@ export function Property(type?: () => Function): Function {
                 [property]: {
                     // Workaround for this issue: https://github.com/microsoft/TypeScript/issues/4521
                     reflectedType: reflectedType || Object,
-                    providedType: type,
+                    providedType: typeOrOption,
                 },
             },
             target,
